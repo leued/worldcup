c7fe0250de2e971a7b5efe408122a8bc0fbb0dd3
lifuen <liufuen@163.com>
2018-05-02 17:01:38 +0800

    fuen

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,56 +1,137 @@
 <template>
   	<div>
-		<div class="g-flexbox wrapbox" v-for="(item,index) in showData" :class="{current:index==currentLine}" >
-			<div class="g-flex box">{{item.left}}</div>
-			<div class="box box_2">
-				<span :class="{on:isOn(index, childrenIndex), on2:isOn2(index, childrenIndex)}"  v-for="(i, childrenIndex) in item.value">{{i.name}}</span>
-			</div>
-			<div class="g-flex box">{{item.right}}</div>
-  		</div>
+		<div domIndex="0" class="time domOn"><span>猜胜负</span></div>
+		<div domIndex="1" class="time">{{showData[currentDataIndex].time}}</div>
+		<div>
+			<div :domIndex="index+2" class="g-flexbox wrapbox" v-for="(i,index) in showData[currentDataIndex].data" >
+				<div class="g-flex box">{{i.left}}</div>
+				<div class="box box_2">
+					<span :class="{on:isOn(index, childrenIndex), on2:isOn2(index, childrenIndex)}"  v-for="(j, childrenIndex) in i.value">{{j.name}}</span>
+				</div>
+				<div class="g-flex box">{{i.right}}</div>
+  			</div>
+		</div>
+		<div :domIndex="currentDataLength()+2">提交</div>
   	</div>
 </template>
 
 <script>
 import Vue from 'vue';
-
+import $ from '../../assets/zepto.min.js';
 export default {
   name: 'index',
   data () {
     return {
-    	showData:[
+		showData:[
 			{
-				left:"秘鲁",
-				right:"丹麦",
-				value:[
-					{name:"胜"},
-					{name:"平"},
-					{name:"负"}
+				time:"20180601",
+				data:[
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					}	
 				]
-			},
-			{
-				left:"秘鲁",
-				right:"丹麦",
-				value:[
-					{name:"胜"},
-					{name:"平"},
-					{name:"负"}
-				]
-			},
-			{
-				left:"秘鲁",
-				right:"丹麦",
-				value:[
-					{name:"胜"},
-					{name:"平"},
-					{name:"负"}
+			},{
+				time:"20180602",
+				data:[
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					},
+					{
+						left:"秘鲁",
+						right:"丹麦",
+						value:[
+							{name:"胜"},
+							{name:"平"},
+							{name:"负"}
+						]
+					}	
 				]
 			}
+		],
+		domIndex:0,          //页面元素索引
+		currentDataIndex:0,  //当前时间索引
+		currentDataLength:function(){
+			return this.showData[this.currentDataIndex].data.length;
+		},
+		currentLineIndex:{   //当前列索引
+			0:{
+				len:3,
+				line:0
+			},
+			1:{
+				len:3,
+				line:0
+			},
+			2:{
+				len:3,
+				line:0
+			}
 
-    	],
-		baseWidth:192,
-		currentLine:0,//上下
-		//currentCol:0,//左右
-		currentCol:{
+		},
+		currentCol:{  //当前行的元素的索引
 			0:{
 				col:0
 			},
@@ -76,63 +157,102 @@ export default {
 				  		me.enterPlay();
 				  		break;
 				    case 37: //左键
-					    me.currentCol[me.currentLine].col--;
-				      	me.setX();
+					    //me.currentCol[me.currentLine].col--;
+				      	me.setX("left");
 				     	break;
 				    case 38: //向上键
-						me.currentLine--;
-				      	me.setY();
+				    	me.setDomY("up")
+				      	//me.setY("up");
 				      break;
 				    case 39: //右键
-				      	me.currentCol[me.currentLine].col++;
-			      		me.setX();
+				      	//me.currentCol[me.currentLine].col++;
+			      		me.setX("right");
 				      	break;
 				    case 40: //向下键
-			      		me.currentLine++;
-			      		me.setY();
+				    	me.setDomY("down")
+			      		//me.setY("down");
 				      	break;
 				    default:
 				      	break;
 			  	}
 			}
 		},
-		setY(){
+		setDomY(type){
 			var me = this;
-			if(me.currentLine<0){
-				me.currentLine=0;
+			if(type=="up"){
+				me.domIndex--;
+			}else{
+				me.domIndex++;
 			}
-			if(me.currentLine >= me.showData.length){
-				me.currentLine = me.showData.length-1;
-			} 
-			me.currentCol[me.currentLine] = {
-				col:me.currentCol[me.currentLine].col ? me.currentCol[me.currentLine].col : 0
+			
+			if(me.domIndex*1<=0){
+				me.domIndex = 0;
+			}
+			if(me.domIndex*1>=$('[domIndex]').length){
+				me.domIndex = $('[domIndex]').length-1;
+			}
+			$('[domIndex]').removeClass('domOn');
+			$('[domIndex="'+me.domIndex+'"]').addClass('domOn');
+
+			if(me.domIndex==2){
 			}
 		},
-		setX(){
+		setY(type){
 			var me = this;
-			if(me.currentCol[me.currentLine].col<0){
-				me.currentCol[me.currentLine].col=0;
+			if(type=="up"){
+				me.currentLineIndex[me.currentDateIndex].line--;
+			}else{
+				me.currentLineIndex[me.currentDateIndex].line++;
 			}
-			if(me.currentCol[me.currentLine].col >= 3){
-				me.currentCol[me.currentLine].col = 2;
+			if(me.currentLineIndex[me.currentDateIndex].line<0){
+				me.currentLineIndex[me.currentDateIndex].line=0;
+			}
+			if(me.currentLineIndex[me.currentDateIndex].line >= me.currentLineIndex[me.currentDateIndex].len){
+				me.currentLineIndex[me.currentDateIndex].line = me.currentLineIndex[me.currentDateIndex].len-1;
 			} 
 
-			me.currentCol[me.currentLine] = {
-				col:me.currentCol[me.currentLine].col
+
+			/*me.currentCol[me.currentLine] = {
+				col:me.currentCol[me.currentLine].col ? me.currentCol[me.currentLine].col : 0
+			}*/
+		},
+		setX(type){
+			var me = this;
+			if(me.domIndex==1 && type=="right"){
+				me.currentDataIndex++
+			}
+			if(me.domIndex==1 && type=="left"){
+				me.currentDataIndex--
 			}
 
+			// if(me.currentCol[me.currentLine].col<0){
+			// 	me.currentCol[me.currentLine].col=0;
+			// }
+			// if(me.currentCol[me.currentLine].col >= 3){
+			// 	me.currentCol[me.currentLine].col = 2;
+			// } 
+			// me.currentCol[me.currentLine] = {
+			// 	col:me.currentCol[me.currentLine].col
+			// }
+		},
+		isCurrent(dateIndex, index){
+			var me  = this;
+			if(dateIndex==me.currentDateIndex && 
+				index==me.currentLineIndex[me.currentDateIndex].line){
+				return "current";
+			}
 		},
 		isOn(index, childrenIndex){
 			var me = this;
-			if(childrenIndex==me.currentCol[me.currentLine].col && index==me.currentLine){
+			/*if(childrenIndex==me.currentCol[me.currentLine].col && index==me.currentLine){
 					return true;
-			}
+			}*/
 		},
 		isOn2(index, childrenIndex){
 			var me = this;
-			if(childrenIndex==me.currentCol[index].col){
+			/*if(childrenIndex==me.currentCol[index].col){
 					return true;
-			}
+			}*/
 		}
   },
   mounted(){
@@ -142,7 +262,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" >
 	.g-flexbox{
 		display:-moz-flex; /* Firefox */
 		display:-webkit-flex; /* Safari and Chrome */
@@ -154,6 +274,13 @@ export default {
 		-webkit-flex:1.0; /* Safari and Chrome */
 		-ms-flex:1.0; /* Internet Explorer 10 */
 		flex:1.0;
+	}
+	.domOn{
+		border: 1px solid red;
+	}
+	.time{
+		width: 400px;
+		margin: 0 auto;	
 	}
 	.wrapbox{
 		width: 400px;
@@ -170,6 +297,9 @@ export default {
 		border: 1px solid blue;
 		text-align: center;
 		font-size: 14px;
+		span{
+			color: red;
+		}
 	}
 	.box_2{
 		border: none;
